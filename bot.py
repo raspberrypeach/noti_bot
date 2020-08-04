228c92ffcd00fd343420ebd897421a2ddbbf39b6
raspberrypeach <n/a>
2020-08-04 15:32:45 +0900

    bot

--- bot.py
+++ bot.py
@@ -1,9 +1,5 @@
-#http://ice.yu.ac.kr/user/indexSub.action?codyMenuSeq=2297360&siteId=ice&menuType=T&uId=6&sortChar=A&linkUrl=6_1.html&mainFrame=right
-#http://ice.yu.ac.kr/user/boardList.action?command=view&page=1&boardId=2559927&boardSeq=24078418
+import feedparser
 
-from urllib.request import urlopen
-import bs4
-url = urlopen("http://ice.yu.ac.kr/user/indexSub.action?codyMenuSeq=2297360&siteId=ice&menuType=T&uId=6&sortChar=A&linkUrl=6_1.html&mainFrame=right")
-bsobj = bs4.BeautifulSoup(url, "url.parser")
-
-print(bsobj.head.title)
+url = "http://ice.yu.ac.kr/rssList.jsp?siteId=ice&boardId=2559927"
+feed = feedparser.parse(url)
+print(feed)
