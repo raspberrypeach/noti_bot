484d1203d31e79fd0d4ea9cd456b2eab867131e1
raspberrypeach <n/a>
2020-08-04 15:36:49 +0900

    bot

--- bot.py
+++ bot.py
@@ -2,4 +2,5 @@ import feedparser
 
 url = "http://ice.yu.ac.kr/rssList.jsp?siteId=ice&boardId=2559927"
 feed = feedparser.parse(url)
-print(feed)
+print(len(feed['entries']))
+print(feed['entries'][0]['published'])
